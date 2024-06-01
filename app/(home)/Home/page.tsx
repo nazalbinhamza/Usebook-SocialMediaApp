dd9802f2967afdf0b792f2dc7eecc02278255d28
Nazal_bin_Hamza <151708027+nazalbinhamza@users.noreply.github.com>
2024-06-01 10:27:26 +0530

    add: new style

--- app/(home)/Home/page.tsx
+++ app/(home)/Home/page.tsx
@@ -214,7 +214,20 @@ function page() {
             <a className="font-semibold">{localStorage.getItem('username')}</a>
                   <a>{item.desc}</a>
           </p>
-        
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6  ml-[90px] mt-[-71px]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
+            />
+          </svg>
           
         </div>
      ))}
@@ -230,7 +243,13 @@ function page() {
               </span>
             </p>
           </div>
-          <div style={{borderRadius:'10px',width:'470px',height:'400px', backgroundImage: `url(${item.image})`, backgroundSize: 'cover', backgroundPosition: 'center',marginTop:'40px',marginLeft:'45px'}}></div>
+          <img
+            src="car4.jpg"
+            className="car-img"
+            onDoubleClick={() => {
+              like ? setLike(false) : setLike(true);
+            }}
+          />
           <svg
             onClick={() => {
               like ? setLike(false) : setLike(true);
@@ -253,9 +272,7 @@ function page() {
           <p className="ml-[53px] mt-[5px]">
             <a className="font-semibold">roadway</a> Set free the beast with the
             Mercedes-AMG ML 63. With a handcrafted AMG V8 biturbo engine under
-            the hood, making it a true powerhouse on the road. And with its
-            sleek, sporty design and luxurious AMG interior, the ML 63 is the
-            perfect combination of style and performance.
+            the hood, making it a true powerhouse on the road.
           </p>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -263,7 +280,7 @@ function page() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            className="w-6 h-6  ml-[90px] mt-[-167px]"
+            className="w-6 h-6  ml-[90px] mt-[-121px]"
           >
             <path
               stroke-linecap="round"
