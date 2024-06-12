f0026df8f3ece91dce08957d707d1c638f3ab096
Nazal_bin_Hamza <151708027+nazalbinhamza@users.noreply.github.com>
2024-06-12 22:04:39 +0530

    add: add dlt items

--- app/shared/SidNav.tsx
+++ app/shared/SidNav.tsx
@@ -52,11 +52,11 @@ function SindNav() {
             d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
           />
         </svg>
-       
+        <Link href={"/Message"}>
           <h5 className="cursor-pointer font-semibold message-text">
             Messages
           </h5>
-      
+        </Link>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -71,11 +71,11 @@ function SindNav() {
             d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
-       
+        <Link href={"/Create"}>
           <h5 className="cursor-pointer font-semibold notification-text">
             Create
           </h5>
-   
+        </Link>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
