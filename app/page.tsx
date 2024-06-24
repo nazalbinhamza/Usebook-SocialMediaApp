d0299afb9dcabc126b5cb56e717bf7ce1b751a5f
Nazal_bin_Hamza <151708027+nazalbinhamza@users.noreply.github.com>
2024-06-24 12:16:44 +0530

    add: some chng

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
           toast.success("Login Successfully");
           setTimeout(() => {
-            window.location.href = "/home";
+            window.location.href = "/Home";
           }, 500);
         }
       } else {
